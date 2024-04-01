82f357e6fd34c1c4940cd5b4558d4a43f370c262
saka0720 <161433692+saka0720@users.noreply.github.com>
2024-04-01 15:48:54 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,10 +1,13 @@
 //変数の宣言
 let bodyWeight;
 let height;
+let bmi;
+
 
 //変数の代入
 bodyWeight=68;
 height=1.7;
+bmi=(bodyWeight/(height*height))
 
 //コンソールへ出力
-console.log (bodyWeight/(height*height))
+console.log (bmi)
